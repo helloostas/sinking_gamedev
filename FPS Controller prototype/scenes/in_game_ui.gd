81b6d1667118e3127af2f55362e4c7510a87bb1a
statsistics <stastigay@gmail.com>
2024-06-12 20:58:05 +1200

    Wed 12 Jun
    - Added card textures
    - UI mockup

---
+++ FPS Controller prototype/scenes/in_game_ui.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+class_name UI
+
+@onready var in_game_ui = $"."
+@onready var score = $Control/MarginContainer/VBoxContainer/HBoxContainer/score
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
