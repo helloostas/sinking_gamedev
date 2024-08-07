950e9a8a00a9757310d1fa3722ba5c8236f34cfd
statsistics <stastigay@gmail.com>
2024-08-08 00:13:19 +1200

    Thu 8 Aug
    Added:
    - Camera tilt on left right movement
    - Title screen demo
    - sliding mechanics

--- FPS Controller prototype/scripts/player.gd
+++ FPS Controller prototype/scripts/player.gd
@@ -2,10 +2,13 @@ extends CharacterBody3D
 
 # Player nodes
 
-@onready var head = $head
+@onready var head = $neck/head
 @onready var standing_collision_shape = $standing_collision_shape
 @onready var crouching_collision_shape = $crouching_collision_shape
 @onready var ray_cast_3d = $RayCast3D
+@onready var camera_3d = $neck/head/Camera3D
+@onready var neck = $neck
+
 
 # Abilities
 var global_abilities = ["double_jump", "dash", "sink", "lunge"]
@@ -14,14 +17,17 @@ var on_hand_abilities = []
 # Speed varibles
 var current_speed = 5.0
 
-const walking_speed = 9.0
-const sprinting_speed = 9.0
-const crounching_speed = 3.0
+const walking_speed = 15.0
+const sprinting_speed = 15.0
+const crounching_speed = 10.0
 
 # States
 var walking = false
 var sprinting = false
 var crouching = false
+var moving_left = false
+var moving_right = false
+var sliding = false
 
 # Movement variables
 const jump_velocity = 6
@@ -29,9 +35,18 @@ const double_jump_velocity = 10
 const sink_velocity = 20
 var crouch_depth = -0.5
 var lerp_speed = 10.0
+var lerp_rotation_speed = 10.0
 var has_dashed = false
 
+var left_move_tilt_ammount = 5
+var right_move_tilt_ammount = -5
 var time = 0
+
+#Slide Variables
+var slide_timer = 0.0
+var slide_timer_max = 1.0
+var slide_vector = Vector2.ZERO
+var slide_speed = 40.0
 
 # Dash Variables
 const dash_velocity = 10
@@ -66,33 +81,42 @@ func _input(event):
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 
 func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	
 	if is_on_floor() and has_dashed:
 		has_dashed = false
 	# Handling movement states
 	
 	# Crouching
 	
-	if Input.is_action_pressed("crounch"):
+	if Input.is_action_pressed("crounch") && is_on_floor():
 		
 		current_speed = crounching_speed
-		head.position.y = lerp(head.position.y, 1.8 + crouch_depth, delta * lerp_speed)
+		head.position.y = lerp(head.position.y,crouch_depth, delta * lerp_speed)
 		
 		standing_collision_shape.disabled = true
 		crouching_collision_shape.disabled = false
 		
-		if not is_on_floor():
-			current_speed = walking_speed
+		
+		
+		#Slide begin logic  NEED TO IMPLEMENT "sprint" function to activate above a certain velocity, right now can be accessed throgh a different way
+		if sprinting && input_dir != Vector2.ZERO:
+			slide_timer = slide_timer_max
+			sliding = true
+			slide_vector = input_dir
 		
 		walking = false
 		sprinting = false
 		crouching = true
+		
 		
 	elif !ray_cast_3d.is_colliding():
 		
 		# Standing
 		standing_collision_shape.disabled = false
 		crouching_collision_shape.disabled = true
-		head.position.y = lerp(head.position.y, 1.8, delta * lerp_speed)
+		head.position.y = lerp(head.position.y, 0.0, delta * lerp_speed)
 		
 		if Input.is_action_pressed("sprint"):
 			
@@ -110,7 +134,38 @@ func _physics_process(delta):
 			walking = true
 			sprinting = false
 			crouching = false
+	
+	#Handle left/right movement
+	if is_on_floor():
+		if Input.is_action_pressed("left") and not moving_right:
+			moving_left = true
+			camera_3d.rotation.z = lerp(camera_3d.rotation.z, deg_to_rad(left_move_tilt_ammount), delta * lerp_rotation_speed)
+		else:
+			moving_left = false
+			camera_3d.rotation.z = lerp(camera_3d.rotation.z, 0.0, delta * lerp_speed)
 
+		if Input.is_action_pressed("right") and not moving_left:
+			moving_right = true
+			camera_3d.rotation.z = lerp(camera_3d.rotation.z, deg_to_rad(right_move_tilt_ammount), delta * lerp_rotation_speed)
+			
+		else:
+			moving_right = false
+			camera_3d.rotation.z = lerp(camera_3d.rotation.z, 0.0, delta * lerp_speed)
+			
+			
+	else:
+		camera_3d.rotation.z = lerp(camera_3d.rotation.z, 0.0, delta * lerp_speed)
+
+
+	#Handle sliding
+	
+	if sliding:
+		slide_timer -= delta
+		if slide_timer <= 0:
+			sliding = false
+			crouching = false
+
+	#Handling the dash
 	if not is_on_floor() and not is_dashing: #and not is_lunging:
 	# Add the gravity.
 		if not has_dashed:
@@ -122,11 +177,13 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
 
-	# Get the input direction and handle the movement/deceleration.
-	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	
 	#lerp speed - will decellerate from whatever speed player is moving at
 	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if sliding:
+		direction = (transform.basis * Vector3(slide_vector.x, 0, slide_vector.y)).normalized()
+		
 	
 	# Tweaking the gravity to maintain horizontal momentum while using "dash"
 	if direction and not is_dashing:
@@ -136,6 +193,10 @@ func _physics_process(delta):
 		else:
 			velocity.x = lerp(velocity.x, direction.x * current_speed, 0.05)
 			velocity.z = lerp(velocity.z, direction.z * current_speed, 0.05)
+		
+		if sliding && is_on_floor():
+			velocity.x = direction.x * slide_timer * slide_speed
+			velocity.z = direction.z * slide_timer * slide_speed
 	else:
 		if is_on_floor():
 			velocity.x = lerp(velocity.x, 0.0, 0.1)
@@ -188,7 +249,7 @@ func _physics_process(delta):
 			elif on_hand_abilities[0] == "lunge":
 				is_lunging = true
 				$lunge_timer.start(lunge_duration)
-				lunge_dir = $head.transform.basis.z
+				lunge_dir = $neck/head.transform.basis.z
 				lunge_dir = transform.basis.z
 				print(on_hand_abilities[0])
 				on_hand_abilities.remove_at(0)
