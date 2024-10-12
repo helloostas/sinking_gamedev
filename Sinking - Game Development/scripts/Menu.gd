b34b99784167820ba3a303f3f0cc6836081eec38
statsistics <stastigay@gmail.com>
2024-10-12 20:23:54 +1300

    Sat 12 Oct
    - Created an options menu

--- Sinking - Game Development/scripts/Menu.gd
+++ Sinking - Game Development/scripts/Menu.gd
@@ -7,7 +7,7 @@ func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/level_one_first_test.tscn")
 
 func _on_options_pressed():
-	get_tree().change_scene_to_file("res://scenes/options.tscn")
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
