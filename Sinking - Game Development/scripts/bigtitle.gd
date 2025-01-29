26cb8e11bd206f823c845d1ebfe050f7e50eff2d
statsistics <stastigay@gmail.com>
2025-01-29 21:35:55 +1300

    Wed 29th Jan
    Published on Itch.io

--- Sinking - Game Development/scripts/bigtitle.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-@onready var timer = $MarginContainer/Timer
-
-func _ready():
-	timer.start()
-
-
-func _on_timer_timeout():
-	get_tree().change_scene_to_file("res://scenes/menu.tscn")
